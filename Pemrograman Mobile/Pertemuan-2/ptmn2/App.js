c38d085310905b40645beda728cfb9474a0f6563
fadhelhusen <fadhelhusen361@gmail.com>
2026-09-21 10:30:44 +0700

    Update App.js

--- Pemrograman Mobile/Pertemuan-2/ptmn2/App.js
+++ Pemrograman Mobile/Pertemuan-2/ptmn2/App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nama: Fadheel Abdillah Husen</Text>
+      <Text>NIM: 2488010013</Text>
+      <Text>Asal Sekolah: MAN 1 CIREBON</Text>
+      <Text>Cita-cita: Game Developer</Text>
+      <Text>Rencana Menggapai cita-cita: Bermain game sambil belajar pemrograman</Text>
       <StatusBar style="auto" />
     </View>
   );
